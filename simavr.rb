ec2cdb459a4f115419536cb01c23adcaa9f41f7f
Jonathan Dehan <jdehan@etsy.com>
2016-02-20 15:09:33 -0500

    simavr: homebrew prefix support patch has been merged

--- simavr.rb
+++ simavr.rb
@@ -6,13 +6,6 @@ class Simavr < Formula
   depends_on "avr-libc"
   depends_on "libelf"
 
-  head do
-    patch :p1 do
-      url "https://patch-diff.githubusercontent.com/raw/buserror/simavr/pull/149.diff"
-      sha256 "2899af128549706da8b964fc3838f9edb729ad37557143a25ec1bdd6da2985b6"
-    end
-  end
-
   def install
     system "make", "install", "DESTDIR=#{prefix}", "HOMEBREW_PREFIX=#{HOMEBREW_PREFIX}", "RELEASE=1"
     prefix.install "examples"
