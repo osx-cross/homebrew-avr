fefd76f718cb68481f4e59c85a0999325fcad0e6
Damien Pollet <damien.pollet@gmail.com>
2016-07-30 11:11:39 +0200

    Update avr-binutils hashes to SHA256
    Looks like SHA1 has been deprecated by Homebrew

--- avr-binutils.rb
+++ avr-binutils.rb
@@ -5,13 +5,13 @@ class AvrBinutils < Formula
     homepage 'http://www.gnu.org/software/binutils/binutils.html'
     url 'http://ftpmirror.gnu.org/binutils/binutils-2.25.tar.gz'
     mirror 'http://ftp.gnu.org/gnu/binutils/binutils-2.25.tar.gz'
-    sha1 'f10c64e92d9c72ee428df3feaf349c4ecb2493bd'
+    sha256 'cccf377168b41a52a76f46df18feb8f7285654b3c1bd69fc8265cb0fc6902f2d'
 
     # Support for -C in avr-size. See issue
     # https://github.com/larsimmisch/homebrew-avr/issues/9
     patch :p0 do
         url 'https://gist.github.com/larsimmisch/4190960/raw/b36f3d6d086980006f097ae0acc80b3ada7bb7b1/avr-binutils-size.patch'
-        sha1 'b6d1ff7084b1f0a3fd2dee5383019ffb202e6c9a'
+        sha256 '1776fa53ec8c8cc6031c205be0ca358db32fccef7831d29d17b3435309ef7a1c'
     end
 
     def install
