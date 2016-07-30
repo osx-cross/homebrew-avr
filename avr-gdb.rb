bb41ce9cde0b47f71d7e393774949a52994f49a8
Dominyk Tiller <dominyktiller@gmail.com>
2016-07-30 12:35:30 +0100

    avr-gdb: update to sha256

--- avr-gdb.rb
+++ avr-gdb.rb
@@ -5,7 +5,7 @@ class AvrGdb < Formula
     homepage 'http://www.gnu.org/software/gdb/'
     url "http://ftp.gnu.org/gnu/gdb/gdb-7.8.2.tar.gz"
     mirror "http://ftpmirror.gnu.org/gnu/gdb/gdb-7.8.2.tar.gz"
-    sha1 '67cfbc6efcff674aaac3af83d281cf9df0839ff9'
+    sha256 'fd9a9784ca24528aac8a4e6b8d7ae7e8cf0784e128cd67a185c986deaf6b9929'
 
     depends_on 'avr-binutils'
 
