b1068e80b6196abd7ae2caa3c713cccb314e6902
Dominyk Tiller <dominyktiller@gmail.com>
2016-07-30 12:36:06 +0100

    avr-gdb: style tweaks

--- avr-gdb.rb
+++ avr-gdb.rb
@@ -1,40 +1,37 @@
-require 'formula'
-
 class AvrGdb < Formula
+  homepage "https://www.gnu.org/software/gdb/"
+  url "https://ftp.gnu.org/gnu/gdb/gdb-7.8.2.tar.gz"
+  mirror "https://ftpmirror.gnu.org/gnu/gdb/gdb-7.8.2.tar.gz"
+  sha256 "fd9a9784ca24528aac8a4e6b8d7ae7e8cf0784e128cd67a185c986deaf6b9929"
 
-    homepage 'http://www.gnu.org/software/gdb/'
-    url "http://ftp.gnu.org/gnu/gdb/gdb-7.8.2.tar.gz"
-    mirror "http://ftpmirror.gnu.org/gnu/gdb/gdb-7.8.2.tar.gz"
-    sha256 'fd9a9784ca24528aac8a4e6b8d7ae7e8cf0784e128cd67a185c986deaf6b9929'
+  depends_on "avr-binutils"
 
-    depends_on 'avr-binutils'
+  def install
+    args = [
+      "--target=avr",
+      "--prefix=#{prefix}",
 
-    def install
-        args = [
-            "--target=avr",
-            "--prefix=#{prefix}",
+      "--disable-nls",
+      "--disable-libssp",
+      "--disable-install-libbfd",
+      "--disable-install-libiberty",
 
-            "--disable-nls",
-            "--disable-libssp",
-            "--disable-install-libbfd",
-            "--disable-install-libiberty",
+      "--with-gmp=#{Formula["gmp"].opt_prefix}",
+      "--with-mpfr=#{Formula["mpfr"].opt_prefix}",
+      "--with-mpc=#{Formula["libmpc"].opt_prefix}",
+      "--with-cloog=#{Formula["cloog"].opt_prefix}",
+      "--with-isl=#{Formula["isl"].opt_prefix}",
+    ]
 
-            "--with-gmp=#{Formula["gmp"].opt_prefix}",
-            "--with-mpfr=#{Formula["mpfr"].opt_prefix}",
-            "--with-mpc=#{Formula["libmpc"].opt_prefix}",
-            "--with-cloog=#{Formula["cloog"].opt_prefix}",
-            "--with-isl=#{Formula["isl"].opt_prefix}"
-        ]
+    mkdir "build" do
+      system "../configure", *args
+      system "make"
 
-        mkdir 'build' do
-            system "../configure", *args
-            system "make"
-
-            ENV.deparallelize
-            system "make install"
-        end
-
-        # info conflicts with binutils
-        info.rmtree
+      ENV.deparallelize
+      system "make", "install"
     end
+
+    # info conflicts with binutils
+    info.rmtree
+  end
 end
