4bf40d832a81f2e137b992a94eae5a065b9f828a
Dominyk Tiller <dominyktiller@gmail.com>
2016-07-30 12:40:50 +0100

    avr-libc: style tweaks

--- avr-libc.rb
+++ avr-libc.rb
@@ -1,33 +1,29 @@
-require 'formula'
-
 class AvrLibc < Formula
+  homepage "http://www.nongnu.org/avr-libc/"
+  url "https://download.savannah.gnu.org/releases/avr-libc/avr-libc-2.0.0.tar.bz2"
+  sha256 "b2dd7fd2eefd8d8646ef6a325f6f0665537e2f604ed02828ced748d49dc85b97"
 
-    url 'http://download.savannah.gnu.org/releases/avr-libc/avr-libc-2.0.0.tar.bz2'
-    homepage 'http://www.nongnu.org/avr-libc/'
-    sha256 'b2dd7fd2eefd8d8646ef6a325f6f0665537e2f604ed02828ced748d49dc85b97'
+  depends_on "avr-gcc"
 
-    depends_on 'avr-gcc'
+  def install
+    ENV.delete "CFLAGS"
+    ENV.delete "CXXFLAGS"
+    ENV.delete "LD"
+    ENV.delete "CC"
+    ENV.delete "CXX"
 
-    def install
-        ENV.delete 'CFLAGS'
-        ENV.delete 'CXXFLAGS'
-        ENV.delete 'LD'
-        ENV.delete 'CC'
-        ENV.delete 'CXX'
+    avr_gcc = Formula["avr-gcc"]
 
-        avr_gcc = Formula['avr-gcc']
+    build = `./config.guess`.chomp
 
-        build = `./config.guess`.chomp
+    system "./configure", "--build=#{build}", "--prefix=#{prefix}", "--host=avr"
+    system "make", "install"
 
-        system "./configure", "--build=#{build}", "--prefix=#{prefix}", "--host=avr"
-        system "make install"
+    avr = File.join prefix, "avr"
 
-        avr = File.join prefix, 'avr'
-
-        # copy include and lib files where avr-gcc searches for them
-        # this wouldn't be necessary with a standard prefix
-        ohai "copying #{avr} -> #{avr_gcc.prefix}"
-        cp_r avr, avr_gcc.prefix
-
-    end
+    # copy include and lib files where avr-gcc searches for them
+    # this wouldn't be necessary with a standard prefix
+    ohai "copying #{avr} -> #{avr_gcc.prefix}"
+    cp_r avr, avr_gcc.prefix
+  end
 end
