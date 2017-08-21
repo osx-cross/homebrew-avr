4b351246700f95019f58a8ce669c36fcb58ba09b
Ladislas de Toldi <ladislas@leka.io>
2017-08-21 08:13:20 +0200

    add avrlibc as a resource to simavr - fix #58

--- simavr.rb
+++ simavr.rb
@@ -1,14 +1,34 @@
 class Simavr < Formula
-  desc "a lean, mean and hackable AVR simulator for linux & OSX "
+  desc "Lean, mean and hackable AVR simulator for linux & OSX "
   homepage "https://github.com/buserror/simavr"
   head "https://github.com/buserror/simavr.git"
 
-  depends_on "avr-libc"
   depends_on "libelf"
+  depends_on "avr-binutils"
+
+  resource "avr-libc" do
+    url "https://download.savannah.gnu.org/releases/avr-libc/avr-libc-2.0.0.tar.bz2"
+    sha256 "b2dd7fd2eefd8d8646ef6a325f6f0665537e2f604ed02828ced748d49dc85b97"
+  end
 
   def install
     system "make", "all", "HOMEBREW_PREFIX=#{HOMEBREW_PREFIX}", "RELEASE=1"
     system "make", "install", "DESTDIR=#{prefix}", "HOMEBREW_PREFIX=#{HOMEBREW_PREFIX}", "RELEASE=1"
     prefix.install "examples"
+
+    resource("avr-libc").stage do
+      ENV.prepend_path "PATH", bin
+
+      ENV.delete "CFLAGS"
+      ENV.delete "CXXFLAGS"
+      ENV.delete "LD"
+      ENV.delete "CC"
+      ENV.delete "CXX"
+
+      build = `./config.guess`.chomp
+
+      system "./configure", "--build=#{build}", "--prefix=#{prefix}", "--host=avr"
+      system "make", "install"
+    end
   end
 end
