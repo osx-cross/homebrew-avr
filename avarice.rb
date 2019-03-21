e7cb831042f4b2dd2b699a2bdb808a46741b549e
Ladislas de Toldi <ladislas@leka.io>
2019-03-21 14:46:28 +0100

    fix avarice building issue - fix #86 & fix #96

--- avarice.rb
+++ avarice.rb
@@ -2,18 +2,18 @@ class Avarice < Formula
   homepage "http://avarice.sourceforge.net/"
   head "https://svn.code.sf.net/p/avarice/code/trunk", :using => :svn
 
-  depends_on "binutils"
+  depends_on "avr-binutils"
   depends_on "hidapi"
   depends_on "automake"
 
   def install
-    chdir "avarice" do
-      system "./Bootstrap"
-      system "./configure", "--disable-debug",
-                            "--disable-dependency-tracking",
-                            "--disable-silent-rules",
-                            "--prefix=#{prefix}"
-      system "make", "install"
-    end
+    system "./Bootstrap"
+    system "./configure",
+      "--disable-debug",
+      "--disable-dependency-tracking",
+      "--disable-silent-rules",
+      "--prefix=#{prefix}"
+    system "make", "install"
+
   end
 end
