e38a93d36772e40bebee231792bf4211e25ccb6b
Ladislas de Toldi <ladislas@detoldi.me>
2021-01-11 12:12:50 +0100

    avr-binutils - version bump: 2.35.1

--- Formula/avr-binutils.rb
+++ Formula/avr-binutils.rb
@@ -2,16 +2,16 @@ class AvrBinutils < Formula
   desc "GNU Binutils for the AVR target"
   homepage "https://www.gnu.org/software/binutils/binutils.html"
 
-  url "https://ftp.gnu.org/gnu/binutils/binutils-2.34.tar.xz"
-  sha256 "f00b0e8803dc9bab1e2165bd568528135be734df3fabf8d0161828cd56028952"
+  url "https://ftp.gnu.org/gnu/binutils/binutils-2.35.1.tar.xz"
+  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.35.1.tar.xz"
+  sha256 "3ced91db9bf01182b7e420eab68039f2083aed0a214c0424e257eae3ddee8607"
+  license all_of: ["GPL-2.0-or-later", "GPL-3.0-or-later", "LGPL-2.0-or-later", "LGPL-3.0-only"]
+
+  livecheck do
+    url :stable
+  end
 
   depends_on "gpatch" => :build if OS.linux?
-
-  bottle do
-    root_url "https://dl.bintray.com/osx-cross/bottles-avr"
-    sha256 "5c95ebe6b2e9a36115ca9ef1debb9dcfb140f65df1ffa2f2ef03e2dbbb676fa8" => :mojave
-    sha256 "42185c4eaa583f5e3985846afadf4abcdf4c5e3e54cc2da288c9cd2d4da8e05c" => :high_sierra
-  end
 
   uses_from_macos "zlib"
 
@@ -32,8 +32,9 @@ class AvrBinutils < Formula
 
       "--disable-nls",
       "--disable-debug",
-      "--disable-dependency-tracking",
       "--disable-werror",
+      "--disable-dependency-tracking",
+      "--enable-deterministic-archives",
     ]
 
     mkdir "build" do
@@ -44,17 +45,5 @@ class AvrBinutils < Formula
     end
 
     info.rmtree # info files conflict with native binutils
-  end
-
-  test do
-    version_output = <<~EOS
-      GNU ar (GNU Binutils) 2.34
-      Copyright (C) 2020 Free Software Foundation, Inc.
-      This program is free software; you may redistribute it under the terms of
-      the GNU General Public License version 3 or (at your option) any later version.
-      This program has absolutely no warranty.
-    EOS
-
-    assert_equal `avr-ar --version`, version_output
   end
 end
