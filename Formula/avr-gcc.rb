fcd39d4443e017bc653fe2bf457e5a781916a75a
Ladislas de Toldi <ladislas@detoldi.me>
2021-01-11 14:11:43 +0100

    avr-gcc@9 - update, clean up formula

--- Formula/avr-gcc.rb
+++ Formula/avr-gcc.rb
@@ -8,11 +8,9 @@ class AvrGcc < Formula
 
   head "https://gcc.gnu.org/git/gcc.git"
 
-  bottle do
-    root_url "https://dl.bintray.com/osx-cross/bottles-avr"
-    rebuild 1
-    sha256 "aaf39cac6784b9d9ecb59ca599201144e4cdb11659d5b3799e811ce710296e2d" => :catalina
-    sha256 "d9a67510da4bfe62827c57238ef506ab8513a6b0fbafa51fd26ea5c9e8c35ab5" => :mojave
+  livecheck do
+    url :stable
+    regex(%r{href=.*?gcc[._-]v?(9(?:\.\d+)+)(?:/?["' >]|\.t)}i)
   end
 
   # The bottles are built on systems with the CLT installed, and do not work
@@ -26,8 +24,10 @@ class AvrGcc < Formula
 
   # automake & autoconf are needed to build from source
   # with the ATMega168pbSupport option.
-  depends_on "automake" => :build
   depends_on "autoconf" => :build
+  depends_on "automake" => :build
+
+  depends_on arch: :x86_64
 
   depends_on "avr-binutils"
 
@@ -35,6 +35,8 @@ class AvrGcc < Formula
   depends_on "isl"
   depends_on "libmpc"
   depends_on "mpfr"
+
+  uses_from_macos "zlib"
 
   # GCC bootstraps itself, so it is OK to have an incompatible C++ stdlib
   cxxstdlib_check :skip
@@ -54,19 +56,15 @@ class AvrGcc < Formula
     end
   end
 
-  def version_suffix
-    if build.head?
-      "HEAD"
-    else
-      version.to_s.slice(/\d/)
-    end
-  end
-
   def install
     # GCC will suffer build errors if forced to use a particular linker.
     ENV.delete "LD"
 
-    # Prevent building documentation to avoid related errors
+    version_suffix = version.major.to_s
+
+    # Even when suffixes are appended, the info pages conflict when
+    # install-info is run so pretend we have an outdated makeinfo
+    # to prevent their build.
     ENV["gcc_cv_prog_makeinfo_modern"] = "no"
 
     languages = ["c", "c++"]
@@ -79,6 +77,7 @@ class AvrGcc < Formula
       --libdir=#{lib}/avr-gcc/#{version_suffix}
 
       --enable-languages=#{languages.join(",")}
+
       --with-ld=#{Formula["avr-binutils"].opt_bin/"avr-ld"}
       --with-as=#{Formula["avr-binutils"].opt_bin/"avr-as"}
 
@@ -87,17 +86,28 @@ class AvrGcc < Formula
       --disable-shared
       --disable-threads
       --disable-libgomp
+      --disable-multilib
 
       --with-dwarf2
       --with-avrlibc
+
+      --with-system-zlib
 
       --with-pkgversion=#{pkgversion}
       --with-bugurl=https://github.com/osx-cross/homebrew-avr/issues
     ]
 
+    # Avoid reference to sed shim
+    args << "SED=/usr/bin/sed"
+
     mkdir "build" do
       system "../configure", *args
-      system "make"
+
+      # Use -headerpad_max_install_names in the build,
+      # otherwise updated load commands won't fit in the Mach-O header.
+      # This is needed because `gcc` avoids the superenv shim.
+      system "make", "BOOT_LDFLAGS=-Wl,-headerpad_max_install_names"
+
       system "make", "install"
     end
 
@@ -122,92 +132,5 @@ class AvrGcc < Formula
       system "./configure", "--build=#{build_config}", "--prefix=#{prefix}", "--host=avr"
       system "make", "install"
     end
-  end
-
-  test do
-    ENV.clear
-
-    hello_c = <<~EOS
-      #define F_CPU 8000000UL
-
-      #include <avr/io.h>
-      #include <util/delay.h>
-
-      int main (void) {
-
-        DDRB |= (1 << PB0);
-
-        while(1) {
-          PORTB ^= (1 << PB0);
-          _delay_ms(500);
-        }
-
-        return 0;
-      }
-    EOS
-
-    hello_c_hex = <<~EOS
-      :10000000209A91E085B1892785B92FEF34E38CE000
-      :0E001000215030408040E1F700C00000F3CFE7
-      :00000001FF
-    EOS
-
-    hello_c_hex.gsub!(/\n/, "\r\n")
-
-    (testpath/"hello.c").write(hello_c)
-
-    system "#{bin}/avr-gcc", "-mmcu=atmega328p", "-Os", "-c", "hello.c", "-o", "hello.c.o", "--verbose"
-    system "#{bin}/avr-gcc", "hello.c.o", "-o", "hello.c.elf"
-    system "avr-objcopy", "-O", "ihex", "-j", ".text", "-j", ".data", "hello.c.elf", "hello.c.hex"
-
-    assert_equal `cat hello.c.hex`, hello_c_hex
-
-    hello_cpp = <<~EOS
-      #define F_CPU 8000000UL
-
-      #include <avr/io.h>
-      #include <util/delay.h>
-
-      int main (void) {
-
-        DDRB |= (1 << PB0);
-
-        uint8_t array[] = {1, 2, 3, 4};
-
-        for (auto n : array) {
-          uint8_t m = n;
-          while (m > 0) {
-            _delay_ms(500);
-            PORTB ^= (1 << PB0);
-            m--;
-          }
-        }
-
-        return 0;
-      }
-    EOS
-
-    hello_cpp_hex = <<~EOS
-      :1000000010E0A0E6B0E0ECE7F0E003C0C895319660
-      :100010000D92A636B107D1F700D000D0CDB7DEB72C
-      :10002000209A8091600090916100A0916200B0914F
-      :10003000630089839A83AB83BC83FE0131969E0162
-      :100040002B5F3F4F41E08191882371F05FEF64E3C4
-      :100050009CE0515060409040E1F700C0000095B135
-      :10006000942795B98150F0CFE217F30761F790E03C
-      :0C00700080E00F900F900F900F9008950B
-      :06007C0001020304000074
-      :00000001FF
-    EOS
-
-    hello_cpp_hex.gsub!(/\n/, "\r\n")
-
-    (testpath/"hello.cpp").write(hello_cpp)
-
-    system "#{bin}/avr-g++", "-mmcu=atmega328p", "-Os", "-c", "hello.cpp", "-o", "hello.cpp.o", "--verbose"
-    system "#{bin}/avr-g++", "hello.cpp.o", "-o", "hello.cpp.elf"
-    system "avr-objcopy", "-O", "ihex", "-j", ".text", "-j", ".data", "hello.cpp.elf", "hello.cpp.hex"
-
-    assert_equal `cat hello.cpp.hex`, hello_cpp_hex
   end
 end
