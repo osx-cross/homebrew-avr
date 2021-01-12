72835a42de5059fa10fbce7831af77c4c1bd41d1
Ladislas de Toldi <ladislas@detoldi.me>
2021-01-12 11:07:16 +0100

    Fix avr-gcc@10
    - remove multilib support
    - remove head
    - remove livecheck
    - add test back
    
    Closes #210.
    
    Signed-off-by: Ladislas de Toldi <ladislas@detoldi.me>

--- Formula/avr-gcc@10.rb
+++ Formula/avr-gcc@10.rb
@@ -6,26 +6,16 @@ class AvrGccAT10 < Formula
   mirror "https://ftpmirror.gnu.org/gcc/gcc-10.2.0/gcc-10.2.0.tar.xz"
   sha256 "b8dd4368bb9c7f0b98188317ee0254dd8cc99d1e3a18d0ff146c855fe16c1d8c"
 
-  license "GPL-3.0"
-
-  head "https://gcc.gnu.org/git/gcc.git"
-
-  livecheck do
-    url :stable
-    regex(%r{href=.*?gcc[._-]v?(9(?:\.\d+)+)(?:/?["' >]|\.t)}i)
-  end
-
-  bottle do
-    root_url "https://github.com/osx-cross/homebrew-avr/releases/download/avr-gcc@10-10.2.0"
-    sha256 "c24cd816cb72e90870fa31c11b26b847848fa7fdd172eb0f99adc91081770b7a" => :catalina
-  end
-
   # The bottles are built on systems with the CLT installed, and do not work
   # out of the box on Xcode-only systems due to an incorrect sysroot.
   pour_bottle? do
     reason "The bottle needs the Xcode CLT to be installed."
     satisfy { MacOS::CLT.installed? }
   end
+
+  keg_only "it might interfere with other version of avr-gcc.\n" \
+           "This is useful if you want to have multiple version of avr-gcc\n" \
+           "installed on the same machine"
 
   option "with-ATMega168pbSupport", "Add ATMega168pb Support to avr-gcc"
 
@@ -93,7 +83,6 @@ class AvrGccAT10 < Formula
       --disable-shared
       --disable-threads
       --disable-libgomp
-      --disable-multilib
 
       --with-dwarf2
       --with-avrlibc
@@ -139,5 +128,81 @@ class AvrGccAT10 < Formula
       system "./configure", "--build=#{build_config}", "--prefix=#{prefix}", "--host=avr"
       system "make", "install"
     end
+  end
+
+  test do
+    ENV.clear
+
+    hello_c = <<~EOS
+      #define F_CPU 8000000UL
+      #include <avr/io.h>
+      #include <util/delay.h>
+      int main (void) {
+        DDRB |= (1 << PB0);
+        while(1) {
+          PORTB ^= (1 << PB0);
+          _delay_ms(500);
+        }
+        return 0;
+      }
+    EOS
+
+    hello_c_hex = <<~EOS
+      :10000000209A91E085B1892785B92FEF34E38CE000
+      :0E001000215030408040E1F700C00000F3CFE7
+      :00000001FF
+    EOS
+
+    hello_c_hex.gsub!(/\n/, "\r\n")
+
+    (testpath/"hello.c").write(hello_c)
+
+    system "#{bin}/avr-gcc", "-mmcu=atmega328p", "-Os", "-c", "hello.c", "-o", "hello.c.o", "--verbose"
+    system "#{bin}/avr-gcc", "hello.c.o", "-o", "hello.c.elf"
+    system "avr-objcopy", "-O", "ihex", "-j", ".text", "-j", ".data", "hello.c.elf", "hello.c.hex"
+
+    assert_equal `cat hello.c.hex`, hello_c_hex
+
+    hello_cpp = <<~EOS
+      #define F_CPU 8000000UL
+      #include <avr/io.h>
+      #include <util/delay.h>
+      int main (void) {
+        DDRB |= (1 << PB0);
+        uint8_t array[] = {1, 2, 3, 4};
+        for (auto n : array) {
+          uint8_t m = n;
+          while (m > 0) {
+            _delay_ms(500);
+            PORTB ^= (1 << PB0);
+            m--;
+          }
+        }
+        return 0;
+      }
+    EOS
+
+    hello_cpp_hex = <<~EOS
+      :1000000010E0A0E6B0E0ECE7F0E003C0C895319660
+      :100010000D92A636B107D1F700D000D0CDB7DEB72C
+      :10002000209A8091600090916100A0916200B0914F
+      :10003000630089839A83AB83BC83FE0131969E0162
+      :100040002B5F3F4F41E08191882371F05FEF64E3C4
+      :100050009CE0515060409040E1F700C0000095B135
+      :10006000942795B98150F0CFE217F30761F790E03C
+      :0C00700080E00F900F900F900F9008950B
+      :06007C0001020304000074
+      :00000001FF
+    EOS
+
+    hello_cpp_hex.gsub!(/\n/, "\r\n")
+
+    (testpath/"hello.cpp").write(hello_cpp)
+
+    system "#{bin}/avr-g++", "-mmcu=atmega328p", "-Os", "-c", "hello.cpp", "-o", "hello.cpp.o", "--verbose"
+    system "#{bin}/avr-g++", "hello.cpp.o", "-o", "hello.cpp.elf"
+    system "avr-objcopy", "-O", "ihex", "-j", ".text", "-j", ".data", "hello.cpp.elf", "hello.cpp.hex"
+
+    assert_equal `cat hello.cpp.hex`, hello_cpp_hex
   end
 end
