0881a8e03952e504dea5f02bf48d0f40267fefe3
osxCrossTestBot <osxCrossTestBot@leka.io>
2021-01-14 16:56:04 +0000

    avarice: add 2.13 bottle.

--- Formula/avarice.rb
+++ Formula/avarice.rb
@@ -6,6 +6,13 @@ class Avarice < Formula
   mirror "https://netix.dl.sourceforge.net/project/avarice/avarice/avarice-2.13/avarice-2.13.tar.bz2"
   sha256 "a14738fe78e1a0a9321abcca7e685a00ce3ced207622ccbcd881ac32030c104a"
 
+  bottle do
+    root_url "https://github.com/osx-cross/homebrew-avr/releases/download/avarice-2.13"
+    cellar :any_skip_relocation
+    rebuild 1
+    sha256 "0b89f6caad256fa89f658069eec9201780579bf76df32214b41933f95d697213" => :catalina
+  end
+
   depends_on "automake"
   depends_on "avr-binutils"
   depends_on "hidapi"
