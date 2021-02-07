83ac4a1ef78941385b083bb895cbd42a44e3f756
Ladislas de Toldi <ladislas@detoldi.me>
2021-02-07 14:59:14 +0100

    avr-gcc@10: revision bump

--- Formula/avr-gcc@10.rb
+++ Formula/avr-gcc@10.rb
@@ -5,6 +5,7 @@ class AvrGccAT10 < Formula
   url "https://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.xz"
   mirror "https://ftpmirror.gnu.org/gcc/gcc-10.2.0/gcc-10.2.0.tar.xz"
   sha256 "b8dd4368bb9c7f0b98188317ee0254dd8cc99d1e3a18d0ff146c855fe16c1d8c"
+  revision 1
 
   bottle do
     root_url "https://github.com/osx-cross/homebrew-avr/releases/download/avr-gcc@10-10.2.0"
