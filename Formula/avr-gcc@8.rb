139b96b9cb515c07192ef73f5675e6fedf6db18c
Ladislas de Toldi <ladislas@detoldi.me>
2025-07-07 10:59:32 +0200

    fix errors

--- Formula/avr-gcc@8.rb
+++ Formula/avr-gcc@8.rb
@@ -7,16 +7,8 @@ class AvrGccAT8 < Formula
   sha256 "d308841a511bb830a6100397b0042db24ce11f642dab6ea6ee44842e5325ed50"
 
   license "GPL-3.0-or-later" => { with: "GCC-exception-3.1" }
-  revision 2
 
   head "https://gcc.gnu.org/git/gcc.git", branch: "releases/gcc-8"
-
-  bottle do
-    root_url "https://github.com/osx-cross/homebrew-avr/releases/download/avr-gcc@8-8.5.0_2"
-    sha256 arm64_sonoma: "e83a6abfe3325d48095b12e66b211bbad5d1f97b3741329745e7d6a2b39b124a"
-    sha256 ventura:      "e0461aa32e2c6e9f4a26e16bf58c46e2a68c11bb660585ccfae4ce8ed56c5328"
-    sha256 monterey:     "00188e6a36f4375908e83b8812448ecfca6049aaee204ecaa07a2b23899432f3"
-  end
 
   # The bottles are built on systems with the CLT installed, and do not work
   # out of the box on Xcode-only systems due to an incorrect sysroot.
@@ -30,8 +22,6 @@ class AvrGccAT8 < Formula
   keg_only "it might interfere with other version of avr-gcc.\n" \
            "This is useful if you want to have multiple version of avr-gcc\n" \
            "installed on the same machine"
-
-  option "with-ATMega168pbSupport", "Add ATMega168pb Support to avr-gcc"
 
   # automake & autoconf are needed to build from source
   # with the ATMega168pbSupport option.
@@ -48,8 +38,6 @@ class AvrGccAT8 < Formula
 
   # GCC bootstraps itself, so it is OK to have an incompatible C++ stdlib
   cxxstdlib_check :skip
-
-  current_build = build
 
   resource "avr-libc" do
     url "https://github.com/avrdudes/avr-libc/releases/download/avr-libc-2_2_1-release/avr-libc-2.2.1.tar.bz2"
@@ -121,8 +109,6 @@ class AvrGccAT8 < Formula
     rm_r(info)
     rm_r(man7)
 
-    current_build = build
-
     resource("avr-libc").stage do
       ENV.prepend_path "PATH", bin
 
@@ -139,7 +125,6 @@ class AvrGccAT8 < Formula
         puts "Forcing build system to aarch64-apple-darwin."
       end
 
-      system "./bootstrap" if current_build.with? "ATMega168pbSupport"
       system "./configure", "--prefix=#{prefix}", "--host=avr"
       system "make", "install"
     end
